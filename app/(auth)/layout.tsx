3cc6cfe405c7a7e8dc5418f4afc081e0c3d71b9c
riad <maksusbinislam@gmail.com>
2023-08-10 23:52:50 +0600

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,32 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+import React from "react";
+
+import { Inter } from 'next/font/google'
+
+import '../globals.css'
+
+
+export const metadata = {
+    title: 'Alaap',
+    description: 'A platform for developer where they can connect with other developer. Share new Ideas , Publish their thoughts and comment on each other posts.'
+}
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({ 
+    children
+ }: { children: React.ReactNode
+}) {
+    return (
+       <ClerkProvider>
+         <html lang="en" >
+                <body className={`${inter.className} bg-dark-1` } >
+                    {children}
+                </body>
+
+            </html>
+
+       </ClerkProvider>
+    )
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,50 @@
+import '../globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/Rightsidebar'
+import Bottombar from '@/components/shared/Bottombar'
+import { ClerkProvider } from "@clerk/nextjs"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Alaap',
+  description: 'A platform for developer where they can connect with other developer. Share new Ideas , Publish their thoughts and comment on each other posts.'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+
+    <ClerkProvider> 
+    <html lang="en">
+      <body className={inter.className}>
+       <Topbar/> 
+
+       <main>
+        <LeftSidebar/> 
+
+        <section className="main-container">
+          <div className="w-full max-w-4xl" >
+            {children}
+          </div>
+        </section>
+
+
+        <RightSidebar />
+       </main>
+
+       <Bottombar />
+
+        
+        {children}</body>
+    </html>
+    </ClerkProvider>
+
+  )
+}
