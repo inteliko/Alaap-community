8461c3a9c503aadf639881b7c702920c25558116
riad <maksusbinislam@gmail.com>
2023-08-16 22:18:18 +0600

    backend

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -24,6 +24,8 @@ import { useUploadThing } from '@/lib/uploadthing';
 
 import * as z from "zod"
 import { ChangeEvent, useState } from 'react';
+import { isBase64Image } from '@/lib/utils';
+import { updateUser } from '@/lib/actions/user.actions';
 
 
 interface props {
@@ -42,6 +44,7 @@ interface props {
 
 const AccountProfile = ({user, btnTitle} : props ) => {
     const [files, setFiles ] = useState<File[]> ([])
+    const { startUpload } = useUploadThing("media");
     
     const form = useForm({
         resolver: zodResolver( UserValidation  ),
@@ -80,10 +83,21 @@ const AccountProfile = ({user, btnTitle} : props ) => {
 
 
      // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof UserValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    const onSubmit = async  (values: z.infer<typeof UserValidation>) =>  {
+        const blob = values.profile_photo;
+
+        const hasImageChanged = isBase64Image(blob);
+
+        if(hasImageChanged) {
+          const imgRes = await startUpload(files)
+
+          if( imgRes && imgRes[0].fileUrl){
+            values.profile_photo = imgRes[0].fileUrl;
+          }
+        }
+
+        // Update user 
+        
     }
 
 
