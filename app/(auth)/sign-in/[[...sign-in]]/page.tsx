ff55475517a3da656746016ddea6b16e6df9ee34
riad <maksusbinislam@gmail.com>
2023-08-27 12:50:24 +0600

    add

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,15 +1,25 @@
 import { SignIn } from "@clerk/nextjs";
 
+
 export default function Page() {
   return (
-   
-    <div>
-        <h1 className='text-heading3-bold text-light-1 max-xs:hidden' >
-           A Community for developer
+    <div className="flex items-center justify-center h-screen bg-black">
+      <div className="text-center px-4">
+        <h1 className="text-heading1-bold bg-clip-text text-transparent bg-gradient-to-r from-violet-500 via-violet-600 to-violet-400 mb-4 text-3xl md:text-4xl lg:text-5xl">
+          Join the Conversation on Alaap
         </h1>
-         <SignIn />
-         
-    </div>
-  ) ;
 
+        <p className="text-light-1 text-lg md:text-xl lg:text-2xl text-center"> {/* Center-align the <p> tag */}
+          Where Developers You can Share Projects,  Ask Questions & Have Discussions.
+        </p>
+        <div className="mt-8">
+          {/* Other content can go here */}
+        </div>
+    
+        <SignIn />
+
+       
+      </div>
+    </div>
+  );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,25 @@
 import { SignUp } from "@clerk/nextjs";
 
+
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <div className="text-center px-4">
+        <h1 className="text-heading1-bold bg-clip-text text-transparent bg-gradient-to-r from-violet-500 via-violet-600 to-violet-400 mb-4 text-3xl md:text-4xl lg:text-5xl">
+          Join the Conversation on Alaap
+        </h1>
+
+        <p className="text-light-1 text-lg md:text-xl lg:text-2xl text-center"> {/* Center-align the <p> tag */}
+          Where Developers you can Share Projects,  Ask Questions & Have Discussions.
+        </p>
+        <div className="mt-8">
+          {/* Other content can go here */}
+        </div>
+    
+        <SignUp />
+
+       
+      </div>
+    </div>
+  );
 }
